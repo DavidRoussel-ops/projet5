d2be79a9321c30d3929067be1bb339a1c146f32e
David Roussel <davidroussel171113@outlook.fr>
2020-08-08 14:23:39 +0200

    javascript teddy

--- ajax.js
+++ ajax.js
@@ -13,3 +13,42 @@ function ajaxGet(url, callback) {
     });
     req.send(null);
 }
+
+function displayTeddies(teddies) {
+    console.log("Affichage de la liste de teddies.")
+    teddies = JSON.parse(teddies)
+    //Création de la div "list-of-teddies"
+    let ListOfTeddies = document.createElement('div')
+    ListOfTeddies.setAttribute('class', 'list-of-teddies row');
+    for (let teddy of teddies){
+        //Création de la grande div de teddy
+        let teddyDiv = document.createElement('div');
+        teddyDiv.setAttribute('class', 'col-2 teddy');
+        ListOfTeddies.appendChild(teddyDiv);
+        // Dans la div teddy, on ajoute les 3 sous-éléments (div, div, img)
+
+        //Première div pour le nom
+        let teddyNameDiv = document.createElement('div');
+        teddyNameDiv.setAttribute('class', 'name');
+        teddyNameDiv.textContent = teddy.name;
+        teddyDiv.appendChild(teddyNameDiv);
+
+        //Deuxième div pour le prix
+        let teddyPriceDiv = document.createElement('div');
+        teddyPriceDiv.setAttribute('class', 'price');
+        teddyPriceDiv.textContent = teddy.price;
+        teddyDiv.appendChild(teddyPriceDiv);
+
+        //Troisième élement de type img pour l'image
+        let teddyPicture = document.createElement('img');
+        teddyPicture.setAttribute('src', teddy.imageUrl);
+        teddyDiv.appendChild(teddyPicture);
+
+        //Quatrième div pour la description
+        let teddyDesc = document.createElement('div');
+        teddyDesc.setAttribute('class', 'descrip');
+        teddyDesc.textContent = teddy.description;
+        teddyDiv.appendChild(teddyDesc);
+    }
+    document.getElementById("teddies").appendChild(ListOfTeddies);
+}
