eb0dfbb86b2096bd257917bb34b1a60ca27e321a
David Roussel <davidroussel171113@outlook.fr>
2020-08-08 14:22:22 +0200

    javascript meuble

---
+++ index.js
@@ -0,0 +1,3 @@
+ajaxGet("http://localhost:3000/api/teddies", displayTeddies)
+ajaxGet( "http://localhost:3000/api/cameras", displayCams)
+ajaxGet("http://localhost:3000/api/furniture", displayMeubles)
