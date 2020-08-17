4daf0887434a1967e5ee015bcfc9bcf897cc3640
David Roussel <davidroussel171113@outlook.fr>
2020-08-17 23:54:42 +0200

    script page produit

--- produit.js
+++ produit.js
@@ -1,72 +1,68 @@
-document.addEventListener("DOMContentLoaded",
-    function (event) {
+//document.addEventListener("DOMContentLoaded",
+    //function (event) {
 
-            const queryString = window.location.search;
+            /*const queryString = window.location.search;
             const urlParams = new
             URLSearchParams(queryString);
-            let objectId = urlParams.get("teddy");
+            let objectId = urlParams.get("teddies_id");
             let type = urlParams.get("typeOfObject");
             console.log("Je recherche l'id" + objectId + "de type" + type);
-            ajaxGet("http://localhost:3000/api/teddies", displayTeddies)
+            //ajaxGet("http://localhost:3000/api/teddies" + teddyId)
+            //let teddyId = JSON.parse(teddies)
 
             function ajaxGet(url, callback) {
-                    let req = new XMLHttpRequest();
-                    req.open("GET", url);
-                    req.addEventListener("load", function () {
-                            if (req.status >= 200 && req.status < 400) {
-                                    callback(req.responseText);
-                            } else {
-                                    console.error(req.status + " " + req.statusText + " " + url);
-                            }
-                    });
-                    req.addEventListener("error", function () {
-                            console.error("Erreur réseau avec l'url" + url);
-                    });
-                    req.send(null);
+            let req = new XMLHttpRequest();
+            req.open("GET", url);
+            req.addEventListener("load", function () {
+                if (req.status >= 200 && req.status < 400){
+                    callback(req.responseText);
+                } else {
+                    console.error(req.status + " " + req.statusText + " " + url);
+                }
+            });
+            req.addEventListener("error", function () {
+                console.error("Erreur réseau avec l'url" + url);
+            });
+            req.send(null);
+        }
+
+        class ted {
+            constructor(id, name, colors, price, description, img) {
+                this.id = id;
+                this.name = name;
+                this.colors = [];
+                this.price = price;
+                this.description = description;
+                this.img = img;
             }
+        }
 
-            function displayTeddies(teddies) {
-                    console.log("Affichage de la liste de teddies.")
-                    teddies = JSON.parse(teddies)
-                    //Création de la div "list-of-teddies"
-                    let ListOfTeddies = document.createElement('div')
-                    ListOfTeddies.setAttribute('class', 'list-of-teddies row');
-                    for (let teddy of teddies) {
-                            //Création de la grande div de teddy
-                            let teddyDiv = document.createElement('div');
-                            teddyDiv.setAttribute('class', 'col-2 teddy');
-                            ListOfTeddies.appendChild(teddyDiv);
-                            // Dans la div teddy, on ajoute les 3 sous-éléments (div, div, img)
+        let norbert = new ted("5be9c8541c9d440000665243", "Norbert", ["Tan","Chocolate","Black","White"], 2900, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://localhost:3000/images/teddy_1.jpg");
+        let arnold = new ted("5beaa8bf1c9d440000a57d94", "Arnold", ["Pale brown","Dark brown","White"], 3900, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://localhost:3000/images/teddy_2.jpg");
 
-                            //Première div pour le nom
-                            let teddyNameDiv = document.createElement('div');
-                            teddyNameDiv.setAttribute('class', 'name');
-                            teddyNameDiv.textContent = teddy.name;
-                            teddyDiv.appendChild(teddyNameDiv);
-
-                            //Deuxième div pour le prix
-                            let teddyPriceDiv = document.createElement('div');
-                            teddyPriceDiv.setAttribute('class', 'price');
-                            teddyPriceDiv.textContent = teddy.price;
-                            teddyDiv.appendChild(teddyPriceDiv);
-
-                            //Troisième élement de type img pour l'image
-                            let teddyPicture = document.createElement('img');
-                            teddyPicture.setAttribute('src', teddy.imageUrl);
-                            teddyDiv.appendChild(teddyPicture);
-
-                            //Quatrième div pour la description
-                            let teddyDesc = document.createElement('div');
-                            teddyDesc.setAttribute('class', 'descrip');
-                            teddyDesc.textContent = teddy.description;
-                            teddyDiv.appendChild(teddyDesc);
-                    }
-
-
-                    let teddyId = document.getElementById("name").innerHTML = displayTeddies().name;
-                    document.getElementById("picture").innerHTML = displayTeddies().imageUrl;
-                    document.getElementById("price").innerHTML = displayTeddies().price;
-                    document.getElementById("description").innerHTML = displayTeddies().description;
+        document.getElementById('name').innerHTML = norbert.name;
+        document.getElementById('picture').innerHTML = norbert.img;
+        document.getElementById('description').innerHTML = norbert.description;
+        document.getElementById('price').innerHTML = norbert.price;
+        document.getElementById('colors').innerHTML = norbert.colors;
+        //document.querySelector(norbert).innerText = 'Nom: ${norbert.name} + Prix: ${norbert.price} + Description: ${norbert.description} + Image produit: ${norbert.img};
+        //console.log(norbert);
+        //document.getElementById("name").innerHTML = ted.name;
+        //document.getElementById("picture").innerHTML = ted.img;
+        //document.getElementById("price").innerHTML = ted.price;
+        //document.getElementById("description").innerHTML = ted.description;
             }
-            document.getElementById("container").appendChild(teddyId);
-    });
+    );*/
+
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let objectId = urlParams.get('id');
+    let type = urlParams.get('type');
+    console.log("je cherche" + objectId + 'de type' + type);
+    ajaxGet("http://localhost:3000/api/" + type + "/" + objectId, displayObject);
+
+})
