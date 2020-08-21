2b6b07469a83da8a059e3108936e0202ab9b1a3d
David Roussel <davidroussel171113@outlook.fr>
2020-08-21 22:23:59 +0200

    essai select

--- unite.js
+++ unite.js
@@ -20,54 +20,54 @@ function displayObject(object) {
 
     //Création de la div "list-of-teddies"
 
-        //Création de la grande div de object
-        let objectDiv = document.createElement('div');
-        objectDiv.setAttribute('class', 'col-2 object');
-        document.getElementById('container').appendChild(objectDiv);
-        // Dans la div object, on ajoute les 3 sous-éléments (div, div, img)
+    //Création de la grande div de object
+    let objectDiv = document.createElement('div');
+    objectDiv.setAttribute('class', 'col-2 object');
+    document.getElementById('container').appendChild(objectDiv);
+    // Dans la div object, on ajoute les 3 sous-éléments (div, div, img)
 
-        //Première div pour le nom
-        let objectNameDiv = document.createElement('div');
-        objectNameDiv.setAttribute('class', 'name');
-        objectNameDiv.textContent = object.name;
-        objectDiv.appendChild(objectNameDiv);
+    //Première div pour le nom
+    let objectNameDiv = document.createElement('div');
+    objectNameDiv.setAttribute('class', 'name');
+    objectNameDiv.textContent = object.name;
+    objectDiv.appendChild(objectNameDiv);
 
-        //Deuxième élement de type img pour l'image
-        let objectPicture = document.createElement('img');
-        objectPicture.setAttribute('src', object.imageUrl);
-        objectDiv.appendChild(objectPicture);
+    //Deuxième élement de type img pour l'image
+    let objectPicture = document.createElement('img');
+    objectPicture.setAttribute('src', object.imageUrl);
+    objectDiv.appendChild(objectPicture);
 
-        //Troisième div pour la description
-        let objectDesc = document.createElement('div');
-        objectDesc.setAttribute('class', 'descrip');
-        objectDesc.textContent = object.description;
-        objectDiv.appendChild(objectDesc);
+    //Troisième div pour la description
+    let objectDesc = document.createElement('div');
+    objectDesc.setAttribute('class', 'descrip');
+    objectDesc.textContent = object.description;
+    objectDiv.appendChild(objectDesc);
 
-        //Quatrième div pour le prix
-        let objectPriceDiv = document.createElement('div');
-        objectPriceDiv.setAttribute('class', 'price');
-        objectPriceDiv.textContent = object.price + '€';
-        objectDiv.appendChild(objectPriceDiv);
-        
-        //Cinquième div pour les couleurs
-        /*let objectColorDiv = document.createElement('div');
-        //let objectColorInput = document.createElement('input');
-        let objectColorSelect = document.createElement('select');
-        let objectColorLabel = document.createElement('legend');
-        //let objectColorOption = document.createElement('option');
-        //objectColorOption.textContent = object.colors.length;
-        objectColorLabel.textContent = 'Selection couleur';
-        //objectColorLabel.appendChild(objectColorOption);
-        objectColorSelect.appendChild(objectColorLabel);
-        objectColorInput.appendChild(objectColorSelect);
-        objectColorDiv.appendChild(objectColorInput);
-        objectDiv.appendChild(objectColorDiv);
+    //Quatrième div pour le prix
+    let objectPriceDiv = document.createElement('div');
+    objectPriceDiv.setAttribute('class', 'price');
+    objectPriceDiv.textContent = object.price + '€';
+    objectDiv.appendChild(objectPriceDiv);
 
-        let objectColorArray = [object.color];
+    //Cinquième div pour les couleurs
+    /*let objectColorDiv = document.createElement('div');
+    //let objectColorInput = document.createElement('input');
+    let objectColorSelect = document.createElement('select');
+    let objectColorLabel = document.createElement('legend');
+    //let objectColorOption = document.createElement('option');
+    //objectColorOption.textContent = object.colors.length;
+    objectColorLabel.textContent = 'Selection couleur';
+    //objectColorLabel.appendChild(objectColorOption);
+    objectColorSelect.appendChild(objectColorLabel);
+    objectColorInput.appendChild(objectColorSelect);
+    objectColorDiv.appendChild(objectColorInput);
+    objectDiv.appendChild(objectColorDiv);
 
-        for (let [i] = 0; i < objectColorArray.length; i++){
-            console.log(objectColorArray[i]);
-    }*/
+    let objectColorArray = [object.color];*/
+
+    /*for (let [i] = 0; i < object.colors.length; i++){
+
+}*/
     let objectColorDiv = document.createElement('div');
     let objectColorSelect = document.createElement('select');
     let objectColorOption = document.createElement('option');
