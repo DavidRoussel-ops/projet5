1218c8d2bf4ccb0e1eb11d3d2776927f09689f20
David Roussel <davidroussel171113@outlook.fr>
2020-08-25 23:19:56 +0200

    test ajout de fonctionalité panier

--- panier.js
+++ panier.js
@@ -2,3 +2,27 @@ let panier = localStorage;
 localStorage.setItem('teddies', 'arnold');
 panier.localStorage.getItem('teddies');
 JSON.parse(panier.localStorage.getItem('teddies'));
+
+//petite fonctionalité de l'icone panier
+
+let timeout;
+
+$('#cart').on({
+    mouseenter: function () {
+        $('#cart-dropdown').show();
+    },
+    mouseleave: function () {
+        timeout = setTimeout(function () {
+            $('#cart-dropdown').hide()
+        }, 200)
+    }
+});
+
+$('#cart-dropdown').on({
+    mouseenter: function () {
+        clearTimeout(timeout);
+    },
+    mouseleave: function () {
+        $('#cart-dropdown').hide();
+    }
+});
