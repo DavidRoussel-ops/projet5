229f4b1832d7e611fc62b954b8b37aed14cb451a
David Roussel <davidroussel171113@outlook.fr>
2020-08-28 17:26:03 +0200

    test panier 4

--- panier.js
+++ panier.js
@@ -1,7 +1,7 @@
-//Création d'une variable récupérant l'élément possédant la classe "add-cart"
-let carts = document.querySelectorAll('.add-cart');
-
-let products = [{"colors":["Tan","Chocolate","Black","White"],"_id":"5be9c8541c9d440000665243","name":"Norbert","price":2900,"imageUrl":"http://localhost:3000/images/teddy_1.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","inCart":0},
+let products = /*function displayObject(object) {
+    object = JSON.parse(object)
+}*/[
+    {"colors":["Tan","Chocolate","Black","White"],"_id":"5be9c8541c9d440000665243","name":"Norbert","price":2900,"imageUrl":"http://localhost:3000/images/teddy_1.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","inCart":0},
     {"colors":["Pale brown","Dark brown","White"],"_id":"5beaa8bf1c9d440000a57d94","name":"Arnold","price":3900,"imageUrl":"http://localhost:3000/images/teddy_2.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","inCart":0},
     {"colors":["Brown"],"_id":"5beaaa8f1c9d440000a57d95","name":"Lenny and Carl","price":5900,"description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","imageUrl":"http://localhost:3000/images/teddy_3.jpg","inCart":0},
     {"colors":["Brown","Blue","Pink"],"_id":"5beaabe91c9d440000a57d96","name":"Gustav","price":4500,"imageUrl":"http://localhost:3000/images/teddy_4.jpg","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","inCart":0},
@@ -18,10 +18,14 @@ let products = [{"colors":["Tan","Chocolate","Black","White"],"_id":"5be9c8541c9
     {"varnish":["Dark Oak","Mahogany"],"_id":"5beab2061c9d440000a57d9b","name":"Vintage Chair","price":79900,"description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","imageUrl":"http://localhost:3000/images/oak_5.jpg","inCart":0}
     ];
 
+//Création d'une variable récupérant l'élément possédant la classe "add-cart"
+let carts = document.querySelectorAll('.add-cart');
+
 //boucle récupérant le nombre de click
 for (let i=0; i < carts.length; i++){
     carts[i].addEventListener('click', () => {
         cartNumbers(products[i]);
+        totalCost(products[i]);
     })
 }
 
@@ -38,6 +42,7 @@ function cartNumbers(product) {
     console.log('le produit selectionner est', product);
     let productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
+
     if (productNumbers){
         localStorage.setItem('cartNumbers', productNumbers + 1);
         document.querySelector('.cart span').textContent = productNumbers + 1;
@@ -47,29 +52,61 @@ function cartNumbers(product) {
 
     }
     setItems(product);
-
 }
 
 function setItems(product) {
     let cartItems = localStorage.getItem('productsInCart');
-    cartItems = JSON.parse(cartItems);
-    if(cartItems != null){
+    JSON.parse(cartItems);
+
+    if (cartItems != null){
         if (cartItems[product._id] === undefined){
             cartItems = {
                 ...cartItems,
                 [product._id]: product
             }
         }
-        cartItems[product._id].inCart += 1;
-    }else {
+            cartItems[product._id].inCart += 1;
+        } else {
         product.inCart = 1;
         cartItems = {
             [product._id]: product
         }
     }
-
-    localStorage.setItem('productsInCart', JSON.stringify
+    localStorage.setItem("productsInCart", JSON.stringify
     (cartItems));
 }
 
+function totalCost(product) {
+    //console.log('le ^prix du produit est', product.price);
+    let cartCost = localStorage.getItem('totalCost');
+
+    console.log('mon cartCost est', cartCost);
+    console.log(typeof cartCost);
+
+    if (cartCost != null){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost + product.price);
+    }else {
+        localStorage.setItem('totalCost', product.price);
+    }
+}
+
+function displayCart() {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector
+    ('.products');
+    let cartCost = localStorage.getItem('totalCost');
+    console.log(cartItems);
+    if (cartItems && productContainer){
+        productContainer.innerHTML = "";
+        Object.values(cartItems).map(item =>{
+            productContainer.innerHTML +='<div class = "product"><ion-icon name="close-circle"></ion-icon><img src="${item.imageUrl}"><span>${item.name}</span></div><div class="price">€${item.price}.00</div><div class="quantity"><ion-icon class="decrease" name="arrow-dropleft-circle"></ion-icon><span>${item.incart}</span><ion-icon class="decrease" name="arrow-dropright-circle"></ion-icon></div><div class="total">€${item.incart * item.price}.00</div>';
+        });
+
+        productContainer.innerHTML += '<div class="basketTotalContainer"><h4 class="basketTotalTitle">Basket Total</h4><h4 class="basketTotal">€${cartCost}.00</h4></div>'
+    };
+}
+
 onLoadCartNumbers();
+displayCart();
