25078e97e31f6f2b25232e0493ea3c32d46747e4
David Roussel <davidroussel171113@outlook.fr>
2020-10-30 14:05:21 +0100

    ajout de promesse

--- projet/js/index.js
+++ projet/js/index.js
@@ -1,3 +1,17 @@
-ajaxGet("http://localhost:3000/api/teddies", displayTeddies)
-ajaxGet( "http://localhost:3000/api/cameras", displayCams)
-ajaxGet("http://localhost:3000/api/furniture", displayMeubles)
+const start = new Promise((resolve, reject) => {
+    ajaxGet("http://localhost:3000/api/teddies", displayTeddies)
+    ajaxGet( "http://localhost:3000/api/cameras", displayCams)
+    ajaxGet("http://localhost:3000/api/furniture", displayMeubles)
+    const starting = true;
+    if (starting === true){
+        resolve()
+    }else {
+        reject()
+    }
+})
+
+start.then(() => {
+    console.log("C'est bon!")
+}).catch(() => {
+    console.log("Oups j'ai fais une erreur...")
+})
